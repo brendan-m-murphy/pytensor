8e9e3e1d535ef6584c2206b4865b4f9e33eaae47
Virgile Andreani <armavica@ulminfo.fr>
2024-04-03 10:53:07 -0400

    Changes for numpy 2.0 deprecations
    - Replace np.cast with np.asarray: in numpy 2.0,
      `np.cast[new_dtype](arr)` is deprecated.
      The literal replacement is `np.asarray(arr, dtype=new_dtype)`.
    
    - Replace np.sctype2char and np.obj2sctype.
      Added try/except to handle change in behavior
      of `np.dtype`
    
    - Replace np.find_common_type with np.result_type

--- pytensor/tensor/type.py
+++ pytensor/tensor/type.py
@@ -101,7 +101,7 @@ class TensorType(CType[np.ndarray], HasDataType, HasShape):
         if str(dtype) == "floatX":
             self.dtype = config.floatX
         else:
-            if np.obj2sctype(dtype) is None:
+            if np.dtype(dtype).type is None:
                 raise TypeError(f"Invalid dtype: {dtype}")
 
             self.dtype = np.dtype(dtype).name
@@ -789,14 +789,16 @@ def tensor(
     **kwargs,
 ) -> "TensorVariable":
     if name is not None:
-        # Help catching errors with the new tensor API
-        # Many single letter strings are valid sctypes
-        if str(name) == "floatX" or (len(str(name)) > 1 and np.obj2sctype(name)):
-            np.obj2sctype(name)
-            raise ValueError(
-                f"The first and only positional argument of tensor is now `name`. Got {name}.\n"
-                "This name looks like a dtype, which you should pass as a keyword argument only."
-            )
+        try:
+            # Help catching errors with the new tensor API
+            # Many single letter strings are valid sctypes
+            if str(name) == "floatX" or (len(str(name)) > 1 and np.dtype(name).type):
+                raise ValueError(
+                    f"The first and only positional argument of tensor is now `name`. Got {name}.\n"
+                    "This name looks like a dtype, which you should pass as a keyword argument only."
+                )
+        except TypeError:
+            pass
 
     if dtype is None:
         dtype = config.floatX
