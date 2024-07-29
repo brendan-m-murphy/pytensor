e2200b9d3425ae92925d9ad18b0b2891e78a3000
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-07-29 09:42:41 +0100

    Make C++ generic get/set real/imag function
    This gives us an easy way to support Numpy v < 2.0,
    and allows the type underlying the bit width types,
    like pytensor_complex128, to be correctly inferred
    from the numpy complex types they inherit from.

--- pytensor/scalar/basic.py
+++ pytensor/scalar/basic.py
@@ -348,6 +348,7 @@ class ScalarType(CType, HasDataType, HasShape):
         l = ["<math.h>"]
         # These includes are needed by ScalarType and TensorType,
         # we declare them here and they will be re-used by TensorType
+
         l.append("<numpy/arrayobject.h>")
         l.append("<numpy/arrayscalars.h>")
         l.append("<numpy/npy_math.h>")
