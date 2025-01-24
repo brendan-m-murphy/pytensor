1a5b97bf968a5c1038fe0652500baf7d2d5cd9f2
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2025-01-24 16:18:45 +0000

    fixup! Updates due to new numpy conversion rules

--- tests/scalar/test_loop.py
+++ tests/scalar/test_loop.py
@@ -208,20 +208,21 @@ def test_inner_composite(mode):
     np.testing.assert_allclose(fn(n_steps=5, x=2.53), 2.53 + 5)
 
     # Now with a dtype that must be rebuilt
-    x32 = float32("x32")
-    y32 = op(n_steps, x32)
-    assert y32.type.dtype == "float32"
+    x16 = float16("x16")
+    y16 = op(n_steps, x16)
+    assert y16.type.dtype == "float16"
 
-    fn32 = function([n_steps, x32], y32, mode=mode)
+    fn16 = function([n_steps, x16], y16, mode=mode)
+    out16 = fn16(n_steps=3, x16=np.array(4.73, dtype="float16"))
     np.testing.assert_allclose(
-        fn32(n_steps=9, x32=np.array(4.73, dtype="float32")),
-        4.73 + 9,
+        out16,
+        4.73 + 3,
         rtol=1e-3,
     )
-    out32overflow = fn32(n_steps=9, x32=np.array(4.73, dtype="float32"))
-    assert out32overflow.dtype == "float32"
+    out16overflow = fn16(n_steps=9, x16=np.array(4.73, dtype="float16"))
+    assert out16overflow.dtype == "float16"
     # with this dtype overflow happens
-    assert np.isnan(out32overflow)
+    assert np.isnan(out16overflow)
 
 
 @mode
